851e81126ca900b466c8cb1df823a95abb53831f
rafael <r_s_carvalho@yahoo.com.br>
2023-06-30 10:36:15 -0300

    Atualização da URL de Produção

--- src/enum/AmbienteEnum.py
+++ src/enum/AmbienteEnum.py
@@ -5,3 +5,4 @@ class AmbienteEnum(Enum):
     """Lista de servidores por ambiente."""
 
     HOMOLOGACAO = 'https://hom-nfoe.estaleiro.serpro.gov.br/API'
+    PRODUCAO = 'https://nfeouro.rfb.gov.br/API'
